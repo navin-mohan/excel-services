5d64c914a645d16810e63a60197627d937d1cc13
Navin Mohan <navinmohan81@gmail.com>
2017-07-13 00:41:20 +0530

    added about us section

--- app/js/main.js
+++ app/js/main.js
@@ -28,7 +28,7 @@ $(document).ready(()=>{
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 30 < 0?0:target.offset().top - 30
         }, 1000);
         return false;
       }
